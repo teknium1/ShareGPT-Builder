b80512e9010ceda3107e462b93680ece81a8e1d9
Teknium <127238744+teknium1@users.noreply.github.com>
2024-02-03 19:38:01 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# ShareGPT Builder
+
+ShareGPT Builder is a simple Flask application that allows you to manually build and store ShareGPT Formatted conversations for training LLM's with a system, a human, and GPT role. The conversations are stored in a JSON file, and automatically append themselves if the data file already exists.
+
+For example datasets using this format, see my [Hermes 2.5 Dataset here](https://huggingface.co/datasets/teknium/OpenHermes-2.5).
+
+## Installation
+
+1. Clone the repository:
+```bash
+git clone https://github.com/teknium1/sharegpt-builder.git
+```  
+
+2. Navigate to the project directory:
+```bash
+cd sharegpt-builder
+```  
+
+3. Install the required Python packages:
+```bash
+pip install flask
+```  
+
+## Usage
+
+1. Run the Flask application:
+```bash
+python app.py
+```  
+
+
+2. Open your web browser and navigate to `http://localhost:5000`.
+
+3. You will see a form with three text areas: one for the system prompt, one for the user prompt, and one for the GPT response. Fill in these fields and click "Submit" to save the conversation to the `data.json` file.
+
+4. To add more turns to the conversation, click the "Add Turn" button. This will add two more text areas: one for the user prompt and one for the GPT response.
+
+5. After adding all the turns, click "Submit" to save the entire conversation.
+
+6. The saved conversations can be viewed in the `data.json` file.
+
+## Contributing
+
+Contributions are welcome and greatly appreciated! Every little bit helps, and credit will always be given.
+
+Here are ways to contribute:
+
+1. Check for open issues or open a fresh issue to start a discussion around a feature idea or a bug.
+2. Fork the repository on GitHub and start making your changes to a new branch.
+3. Write a test which shows that the bug was fixed or that the feature works as expected.
+4. Send a pull request and bug the maintainer until it gets merged and published.
+
+Alternatively, you can contribute via submission of bugs or feature requests to the issues tab.
+
+## Note
+
+The application is set to run in debug mode. For production use, make sure to turn off debug mode in `app.py`.
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+
+app = Flask(__name__)
+
+# Route for index/main page
+@app.route('/', defaults={'active_tab': 'sft'})
+@app.route('/<active_tab>')
+def index(active_tab):
+    return render_template('index.html', active_tab=active_tab)
+
+
+# Route for the SFT Dataset Builder.
+@app.route('/sft', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        # Extract form data
+        system_prompt = request.form.get('system')
+        user_prompts = request.form.getlist('user[]')
+        gpt_responses = request.form.getlist('gpt[]')
+
+        # Data to be appended
+        data_to_append = {
+            'conversations': [
+                {
+                    'from': 'system',
+                    'value': system_prompt
+                }
+            ],
+            'source': 'manual'
+        }
+
+        # Add turns to the conversation
+        for user_prompt, gpt_response in zip(user_prompts, gpt_responses):
+            data_to_append['conversations'].append({
+                'from': 'human',
+                'value': user_prompt
+            })
+            data_to_append['conversations'].append({
+                'from': 'gpt',
+                'value': gpt_response
+            })
+
+        # File path
+        file_path = './data.json'
+
+        # Check if file exists and append data
+        if os.path.exists(file_path):
+            with open(file_path, 'r+', encoding='utf-8') as file:
+                data = json.load(file)
+                data.append(data_to_append)
+                file.seek(0)
+                json.dump(data, file, indent=4)
+        else:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump([data_to_append], file, indent=4)
+
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+# Route for the DPO dataset builder
+@app.route('/dpo', methods=['GET', 'POST'])
+def dpo_form():
+    if request.method == 'POST':
+        # Extract form data
+        system_prompt = request.form.get('system')
+        prompt = request.form.get('prompt')
+        chosen = request.form.get('chosen')
+        rejected = request.form.getlist('rejected[]')
+
+        # Data to be appended
+        data_to_append = {
+            'system': system_prompt,
+            'question': prompt,
+            'chosen': chosen,
+            'rejected': rejected,
+            'source': 'dpo'
+        }
+
+        # File path
+        file_path = './dpo.json'
+
+        # Check if file exists and append data
+        if os.path.exists(file_path):
+            with open(file_path, 'r+', encoding='utf-8') as file:
+                data = json.load(file)
+                data.append(data_to_append)
+                file.seek(0)
+                json.dump(data, file, indent=4)
+        else:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump([data_to_append], file, indent=4)
+
+        return "Success", 200
+    return render_template('index.html', active_tab='dpo')
+
+if __name__ == '__main__':
+    app.run(debug=True)
