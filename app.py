a55c48bc52b20a824dc2f276e1e43b5b57fdb0bc
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-03-17 14:27:32 +0530

    Adding JSON viewer, and more refactors

--- app.py
+++ app.py
@@ -6,98 +6,61 @@ app = Flask(__name__)
 def clean_entry(entry):
     return entry.strip().replace("\r", "").replace(" \n", "\n")
 
-# Route for index/main page
 @app.route('/', defaults={'active_tab': 'sft'})
 @app.route('/<active_tab>')
 def index(active_tab):
     return render_template('index.html', active_tab=active_tab)
 
-# Route for the SFT Dataset Builder.
-@app.route('/sft', methods=['GET', 'POST'])
+@app.route('/sft', methods=['POST'])
 def form():
-    if request.method == 'POST':
-        # Extract form data
-        system_prompt = clean_entry(request.form.get('system'))
-        user_prompts = [clean_entry(prompt) for prompt in request.form.getlist('user[]')]
-        gpt_responses = [clean_entry(response) for response in request.form.getlist('gpt[]')]
-
-        data_to_append = {
-            'conversations': [{'from': 'system', 'value': system_prompt}],
-            'source': 'manual'
-        }
-
-        for user_prompt, gpt_response in zip(user_prompts, gpt_responses):
-            data_to_append['conversations'].append({'from': 'human', 'value': user_prompt})
-            data_to_append['conversations'].append({'from': 'gpt', 'value': gpt_response})
-
-        file_path = './sft_data.json'
-
-        if os.path.exists(file_path):
-            with open(file_path, 'r+', encoding='utf-8') as file:
-                data = json.load(file)
-                data.append(data_to_append)
-                file.seek(0)
-                json.dump(data, file, indent=4)
-        else:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump([data_to_append], file, indent=4)
-
+    system_prompt = clean_entry(request.form.get('system'))
+    user_prompts = [clean_entry(prompt) for prompt in request.form.getlist('user[]')]
+    gpt_responses = [clean_entry(response) for response in request.form.getlist('gpt[]')]
+    data_to_append = {
+        'conversations': [{'from': 'system', 'value': system_prompt}],
+        'source': 'manual'
+    }
+    for user_prompt, gpt_response in zip(user_prompts, gpt_responses):
+        data_to_append['conversations'].extend([
+            {'from': 'human', 'value': user_prompt},
+            {'from': 'gpt', 'value': gpt_response}
+        ])
+    addJsonData('./sft_data.json', data_to_append)
     return redirect(url_for('index', active_tab='sft'))
 
-# Route for the DPO dataset builder
-@app.route('/dpo', methods=['GET', 'POST'])
+@app.route('/dpo', methods=['POST'])
 def dpo_form():
-    if request.method == 'POST':
-        # Extract form data
-        data_to_append = {
-            'system': clean_entry(request.form.get('system')),
-            'question': clean_entry(request.form.get('prompt')),
-            'chosen': clean_entry(request.form.get('chosen')),
-            'rejected': clean_entry(request.form.get('rejected')),
-            'source': 'manual'
-        }
+    data_to_append = {
+        'system': clean_entry(request.form.get('system')),
+        'question': clean_entry(request.form.get('prompt')),
+        'chosen': clean_entry(request.form.get('chosen')),
+        'rejected': clean_entry(request.form.get('rejected')),
+        'source': 'manual'
+    }
+    addJsonData('./dpo_data.json', data_to_append)
+    return "Success", 200
 
-        file_path = './dpo_data.json'
+def addJsonData(file_path, data_to_append):
+    if os.path.exists(file_path):
+        with open(file_path, 'r+', encoding='utf-8') as file:
+            data = json.load(file)
+            data.append(data_to_append)
+            file.seek(0)
+            json.dump(data, file, indent=4)
+    else:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump([data_to_append], file, indent=4)
 
-        if os.path.exists(file_path):
-            with open(file_path, 'r+', encoding='utf-8') as file:
-                data = json.load(file)
-                data.append(data_to_append)
-                file.seek(0)
-                json.dump(data, file, indent=4)
-        else:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump([data_to_append], file, indent=4)
-
-        return "Success", 200
-
-    return render_template('index.html', active_tab='dpo')
-
-@app.route('/sft_data.json', methods=['GET', 'PUT'])
-def sft_data():
-    file_path = './sft_data.json'
+@app.route('/<type>_data.json', methods=['GET', 'PUT'])
+def data(type):
+    file_path = f'./{type}_data.json'
     if request.method == 'GET':
         with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        return jsonify(data)
-    elif request.method == 'PUT':
-        data = request.get_json()
+            return jsonify(json.load(file))
+    else:
         with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4)
-        return jsonify({'message': 'SFT data updated successfully'})
-
-@app.route('/dpo_data.json', methods=['GET', 'PUT'])
-def dpo_data():
-    file_path = './dpo_data.json'
-    if request.method == 'GET':
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        return jsonify(data)
-    elif request.method == 'PUT':
-        data = request.get_json()
-        with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4)
-        return jsonify({'message': 'DPO data updated successfully'})
+            json.dump(request.get_json(), file, indent=4)
+        return jsonify({'message': f'{type.upper()} data updated successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=7272)
--- static/script.js
+++ static/script.js
@@ -12,18 +12,11 @@ function submitForm(e, tabName) {
     e.preventDefault();
     const form = document.getElementById(tabName === 'sft' ? 'sftForm' : 'dpoForm');
     const formElements = form.elements;
-    let allFieldsFilled = true;
+    const allFieldsFilled = Array.from(formElements).every(el => el.tagName !== 'TEXTAREA' || el.value.trim() !== '');
 
     if (tabName === 'sft' && document.getElementsByClassName('turn').length <= 1) {
         showToast("Please add at least one turn.", 'error');
         return;
-    }
-
-    for (let i = 0; i < formElements.length; i++) {
-        if (formElements[i].tagName === 'TEXTAREA' && formElements[i].value.trim() === '') {
-            allFieldsFilled = false;
-            break;
-        }
     }
 
     if (allFieldsFilled) {
@@ -33,9 +26,11 @@ function submitForm(e, tabName) {
         })
         .then(response => {
             if (response.ok) {
-                openTab(tabName);
-                history.pushState({}, "", "/");
                 form.reset();
+                if (tabName === 'sft') {
+                    resetSFTForm();
+                }
+                openJsonModal(tabName);
                 showToast("Submitted successfully!", 'success');
             } else {
                 showToast("Form submission failed.", 'error');
@@ -47,16 +42,20 @@ function submitForm(e, tabName) {
     }
 }
 
+function resetSFTForm() {
+    const turns = document.getElementById('turns');
+    const turnElements = turns.getElementsByClassName('turn');
+    while (turnElements.length > 1) {
+        turnElements[turnElements.length - 1].remove();
+    }
+}
+
 function openTab(tabName) {
     const tabcontent = document.getElementsByClassName("tabcontent");
-    for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    Array.from(tabcontent).forEach(el => el.style.display = "none");
 
     const tablinks = document.getElementsByClassName("tablink");
-    for (let i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    Array.from(tablinks).forEach(el => el.className = el.className.replace(" active", ""));
 
     document.getElementById(tabName).style.display = "block";
     const buttonElement = document.querySelector(`.tablink[onclick="openTab('${tabName}', this)"]`);
@@ -64,8 +63,7 @@ function openTab(tabName) {
         buttonElement.className += " active";
     }
 
-    const jsonModal = document.getElementById('jsonModal');
-    jsonModal.style.display = 'none';
+    openJsonModal(tabName);
 }
 
 document.getElementById("defaultOpen").click();
@@ -94,62 +92,45 @@ function deleteTurn(button) {
     turn.remove();
 }
 
-function openJsonModal(type) {
-    const jsonModal = document.getElementById('jsonModal');
+async function openJsonModal(type) {
     const jsonViewer = document.getElementById('jsonViewer');
     const editJsonBtn = document.getElementById('editJsonBtn');
     const saveJsonBtn = document.getElementById('saveJsonBtn');
 
-    fetch(`/${type}_data.json`)
-        .then(response => response.json())
-        .then(data => {
-            jsonViewer.textContent = JSON.stringify(data, null, 2);
-            hljs.highlightElement(jsonViewer);
-            jsonModal.style.display = 'flex';
+    try {
+        const response = await fetch(`/${type}_data.json`);
+        const data = await response.json();
+        jsonViewer.textContent = JSON.stringify(data, null, 2);
+        hljs.highlightElement(jsonViewer);
 
-            editJsonBtn.onclick = () => {
-                jsonViewer.contentEditable = true;
-                jsonViewer.focus();
-                editJsonBtn.style.display = 'none';
-                saveJsonBtn.style.display = 'inline-block';
-            };
+        editJsonBtn.onclick = () => {
+            jsonViewer.contentEditable = true;
+            jsonViewer.focus();
+            editJsonBtn.style.display = 'none';
+            saveJsonBtn.style.display = 'inline-block';
+        };
 
-            saveJsonBtn.onclick = () => {
+        saveJsonBtn.onclick = async () => {
+            try {
                 const updatedData = JSON.parse(jsonViewer.textContent);
-                fetch(`/${type}_data.json`, {
+                await fetch(`/${type}_data.json`, {
                     method: 'PUT',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(updatedData, null, 2)
-                })
-                .then(() => {
-                    jsonViewer.contentEditable = false;
-                    editJsonBtn.style.display = 'inline-block';
-                    saveJsonBtn.style.display = 'none';
-                    showToast('JSON data updated successfully!', 'success');
-                })
-                .catch(error => {
-                    console.error('Error updating JSON data:', error);
-                    showToast('Failed to update JSON data.', 'error');
                 });
-            };
-        })
-        .catch(error => {
-            console.error('Error fetching JSON data:', error);
-            showToast('Failed to fetch JSON data.', 'error');
-        });
-}
-
-const closeBtn = document.getElementsByClassName('close')[0];
-closeBtn.onclick = () => {
-    const jsonModal = document.getElementById('jsonModal');
-    jsonModal.style.display = 'none';
-};
-
-window.onclick = (event) => {
-    const jsonModal = document.getElementById('jsonModal');
-    if (event.target === jsonModal) {
-        jsonModal.style.display = 'none';
+                jsonViewer.contentEditable = false;
+                editJsonBtn.style.display = 'inline-block';
+                saveJsonBtn.style.display = 'none';
+                showToast('JSON data updated successfully!', 'success');
+            } catch (error) {
+                console.error('Error updating JSON data:', error);
+                showToast('Failed to update JSON data.', 'error');
+            }
+        };
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+        showToast('Failed to fetch JSON data.', 'error');
     }
-};
+}
