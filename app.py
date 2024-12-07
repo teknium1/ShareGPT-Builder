4119292eb91f66a8782fe47a58d9fcd29c4d4318
Lain <hhichri60@gmail.com>
2024-12-07 16:38:59 +0100

    update sft tab

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from huggingface_hub.utils._auth import get_token
-from huggingface_hub import InferenceClient, whoami
+from huggingface_hub import whoami
+import datetime
 
 from dataset_uploader import ParquetScheduler
 
@@ -8,36 +9,17 @@ from dataset_uploader import ParquetScheduler
 hf_token = get_token()
 
 contributor_username = whoami()["name"]
+show_info = True
+
+every = 5 # we push once every 5 minutes
 
 # IMPORTANT !!!
 # change these values
 # repo to where we push the data
-sft_scheduler = ParquetScheduler(repo_id="not-lain/sft",every=5)
+sft_scheduler = ParquetScheduler(repo_id="not-lain/sft", every=every)
 
 
-show_info = True
 
-def generate(messages):
-    """
-    A function that generates a response to a given prompt. 
-    Args:
-        messages: A list of strings, each string being a message from the user or the assistant.
-    Returns:
-        A generator of strings being a response from the assistant.
-    """
-    
-    # we do not need to explecitly pass it if we're already logged in
-    client = InferenceClient(api_key=hf_token)
-
-    stream = client.chat.completions.create(
-        model="Qwen/Qwen2.5-Coder-32B-Instruct",
-        messages=messages,
-        max_tokens=1024,
-        stream=True,
-    )
-
-    for chunk in stream:
-        yield (chunk.choices[0].delta.content)
 
 
 def chat_message(role, content):
@@ -49,28 +31,24 @@ def chat_message(role, content):
     Returns:
         A dictionary, containing the role and the content of the message.
     """
-    
+
     return {"role": role, "content": content}
 
 
 def sft_chat(prompt: str, history=[]):
     """
-    A function that generates a response to a given prompt. 
+    A function that generates a response to a given prompt.
     Args:
         prompt: A string, the prompt to be sent to the chatbot.
         history: A list of dictionaries, each dictionary being a message from the user or the assistant.
     Returns:
         A generator in the form of a single updated list of dictionaries, being a list of messages from the user and assistant
     """
-    
-    history.append(chat_message("user", prompt))
-    yield history
-    out = ""
-    history.append(chat_message("assistant", out))
-    for token in generate(history):
-        out += token
-        history[-1] = chat_message("assistant", out)
-        yield history
+    if history == [] or (len(history) > 1 and history[-1]["role"] == "assistant"):
+        history.append(chat_message("user", prompt))
+    else:
+        history.append(chat_message("assistant", prompt))
+    return history
 
 
 def clear_textbox_field():
@@ -87,41 +65,62 @@ def clear_both_fields():
     return None, None
 
 
-def save_sft_data(history):
+def save_sft_data(system_prompt="", history=[]):
     """
     A function that pushes the data to the hub.
     """
     # setup the info message to only show once
-    global show_info 
+    global show_info
 
+    # case user clicked submit and did not have any chat history
+    if history == []:
+        gr.Error("you need to setup a chat first")
+        return
     # preparing the submission
-    data = {"contributor" : contributor_username}
-    data["model_name"] = "Qwen/Qwen2.5-Coder-32B-Instruct"
+    data = {"contributor": contributor_username}
+    # add system prompt if provided
+    system_prompt = system_prompt.strip()
+    if system_prompt != "":
+        history.insert(0, chat_message("system", system_prompt))
+    
+    data["timestamp"] = datetime.datetime.utcnow().isoformat()
     data["conversations"] = history
 
     # submitting the data
     sft_scheduler.append(data)
-    
-    # show the info message only once 
+
+    # show the info message only once
     if show_info:
         gr.Info("Data has been saved successfully (this message is only shown once)")
-        gr.Info("The scheduler may take up to 5 minutes to push the data, please wait 🤗")
+        gr.Info(
+            "The scheduler may take up to 5 minutes to push the data, please wait 🤗"
+        )
         show_info = False
+
+
+def undo_chat(history):
+    return history[:-2]
 
 
 with gr.Blocks() as demo:
     with gr.Tab("sft"):
-        chatbot = gr.Chatbot(type="messages")
+        with gr.Accordion("system prompt", open=False):
+            system_prompt = gr.TextArea(show_label=False, container=False)
+        
+        chatbot = gr.Chatbot(
+            type="messages", show_copy_button=True, show_copy_all_button=True
+        )
         textbox = gr.Textbox(show_label=False, submit_btn=True)
         textbox.submit(
             fn=sft_chat, inputs=[textbox, chatbot], outputs=[chatbot]
         ).then(  # empty field for convinience
             clear_textbox_field, outputs=[textbox]
         )
+        chatbot.undo(undo_chat, inputs=chatbot, outputs=chatbot)
         with gr.Row():
             clear_button = gr.Button("Clear")
             clear_button.click(clear_both_fields, outputs=[textbox, chatbot])
             submit = gr.Button("save chat", variant="primary")
-            submit.click(save_sft_data, inputs=[chatbot])
+            submit.click(save_sft_data, inputs=[system_prompt, chatbot])
 
 demo.launch(debug=True, show_error=True)
